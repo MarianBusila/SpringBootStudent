c0fc662520ece10c6128e2c4ccba9e41dd03253c
mbusila-unity3d <marian.busila@unity3d.com>
2022-12-19 12:01:33 -0500

    implement PUT

--- student/src/main/java/com/example/student/student/StudentService.java
+++ student/src/main/java/com/example/student/student/StudentService.java
@@ -3,7 +3,9 @@ package com.example.student.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -32,5 +34,17 @@ public class StudentService {
         if(!exists)
             throw new IllegalStateException("student with id " + id + " does not exist");
         studentRepository.deleteById(id);
+    }
+
+    @Transactional
+    public void updateStudent(Long id, String name, String email) {
+        Student student = studentRepository.findById(id)
+                .orElseThrow( () -> new IllegalStateException("student with id " + id + " does not exist"));
+
+        if(name != null && name.length() > 0 && !Objects.equals(student.getName(), name))
+            student.setName(name);
+
+        if(email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email))
+            student.setEmail(email);
     }
 }
